e9dc619d47e5cc07fd9ddfc93f57fffa66e8e043
Akshat Sethi <masterakshata@gmail.com>
2021-06-03 10:36:55 +0530

    add sign in link to sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import SignUpForm from '../components/SignUpForm';
 
 const SignUp = () => (
   <div className="container">
     <h1 className="text-center"> Sign up</h1>
     <SignUpForm />
+    <Link to="/signin" href="/signin">Already have an account? Sign in here</Link>
   </div>
 );
 
