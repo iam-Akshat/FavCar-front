c873a2965b877cb0b4f5a7e9559549b894345d69
Akshat Sethi <masterakshata@gmail.com>
2021-06-06 02:09:01 +0530

    rename authSlice.js to auth.js

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
+import authReducer from './slices/auth';
 
 export default configureStore({
   reducer: { auth: authReducer },
