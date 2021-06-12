873d9b87d72dfb7c85b5d0014f6ad13bbadf40f3
Akshat Sethi <masterakshata@gmail.com>
2021-06-13 03:51:13 +0530

    change BASE_URL according to node environment

--- src/utils/authFetch.js
+++ src/utils/authFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authFetch = axios.create({
-  baseURL: 'https://desolate-earth-04875.herokuapp.com/',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.01:3000' : 'https://desolate-earth-04875.herokuapp.com/',
   headers: {
     'Content-Type': 'Application/json',
   },
