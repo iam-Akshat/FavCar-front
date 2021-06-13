934748a205eb43abdc7cdd2708d4b3aebf79f18e
Akshat Sethi <masterakshata@gmail.com>
2021-06-13 21:24:42 +0530

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,18 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler remove-border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
-        <a className="navbar-brand" href="/">{`Hi ${name}`}</a>
+        <a className="navbar-brand" href="/">Cars</a>
+        <a className="navbar-brand" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </a>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mt-2">{`${name}'s space`}</li>
             <li className="nav-item">
               <Link to="/favourites" href="/favourites" className="nav-link"> Favourites </Link>
             </li>
