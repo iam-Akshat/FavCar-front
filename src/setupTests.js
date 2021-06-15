e2772eeeb886637c394dc98af195a7618fdf6284
Akshat Sethi <masterakshata@gmail.com>
2021-06-15 16:24:15 +0530

    add test for sign up form

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,33 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AuthProvider } from './context/authContext';
+
+global.appRenderer = (renderComponent, store, route) => {
+  const history = createMemoryHistory();
+  const qc = new QueryClient();
+  if (route) {
+    history.push(route);
+  }
+
+  return {
+    ...render(
+      <Provider store={store}>
+        <AuthProvider>
+          <Router history={history}>
+            <QueryClientProvider client={qc}>
+              {renderComponent}
+            </QueryClientProvider>
+          </Router>
+        </AuthProvider>
+
+      </Provider>,
+    ),
+    history,
+  };
+};
